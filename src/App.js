51124c4bf4564e8d4c84daaa101018c1e7f76d84
Adam McElroy <adam.mcelroy15@gmail.com>
2024-03-11 18:20:13 +0000

    added signin and reroute to dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./Signin";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
-    <div className="App">
-      <h1>MERN Stack App</h1>
-      {/* Add your components and routes here */}
-    </div>
+    <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      {/* Add more Route components for other routes */}
+    </Routes>
   );
 }
 
---
+++ src/Signin.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+export default function Signin() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("http://localhost:5000/api/signin", formData);
+      console.log("Signin Success:", res.data);
+      navigate("/dashboard");
+      // redirect to dash
+    } catch (error) {
+      setError(error.response.data.msg);
+    }
+  };
+
+  return (
+    <>
+      <h2>Sign In</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <button type="submit">Sign In</button>
+      </form>
+    </>
+  );
+}
